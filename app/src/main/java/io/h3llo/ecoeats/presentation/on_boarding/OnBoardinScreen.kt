f0dd32deb84a1f495b4033155702004e8970a12b
beto <betolix@gmail.com>
2024-08-28 14:27:39 -0400

    OnBoardingScreen basic implementation. NAVIGATION COMPOSE dependency added toi gradle

--- app/src/main/java/io/h3llo/ecoeats/presentation/on_boarding/OnBoardinScreen.kt
+++ app/src/main/java/io/h3llo/ecoeats/presentation/on_boarding/OnBoardinScreen.kt
@@ -1,2 +1,21 @@
 package io.h3llo.ecoeats.presentation.on_boarding
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.h3llo.ecoeats.presentation.common.TextBasic
+import io.h3llo.ecoeats.presentation.preview.PreviewDefault
+
+@Composable
+fun OnBoardingScreen(modifier: Modifier = Modifier) {
+    Text(text = "OnBoardingscreen")
+    
+}
+
+@PreviewDefault
+@Composable
+private fun OnBoardingScreenPreview() {
+
+    OnBoardingScreen()
+}
