95b661bd3fc9c48ac8e25d0e780fb7821e1e9b1d
beto <betolix@gmail.com>
2024-12-02 15:18:44 -0500

    Navigation Example
    Using objects and data classes for passing arguments

--- app/src/main/java/io/h3llo/ecoeats/navigation/ExampleNavigation.kt
+++ app/src/main/java/io/h3llo/ecoeats/navigation/ExampleNavigation.kt
@@ -3,17 +3,16 @@ package io.h3llo.ecoeats.navigation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
 
 @Composable
 fun ExampleNavigation() {
@@ -22,16 +21,28 @@ fun ExampleNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = "screen1"
+        //startDestination = "screen1"
+        startDestination = MyScreen1
     ){
 
-        composable(route = "screen1") {
+        composable<MyScreen1>{
             Screen1(onClick = {
-                navController.navigate("screen2/${2}")
-
+                navController.navigate(MyScreen2(id = 4))
             })
         }
 
+        composable<MyScreen2> {navBackStackEntry ->
+            val data = navBackStackEntry.toRoute<MyScreen2>()
+            Screen2(id = data.id )
+
+        }
+
+        /*
+        composable(route = "screen1") {
+            Screen1(onClick = {
+                navController.navigate("screen2/${2}")
+            })
+        }
         composable(
             route = "screen2/{id}",
             arguments = listOf(navArgument("id"){ type = NavType.IntType})
@@ -39,12 +50,15 @@ fun ExampleNavigation() {
             val id = requireNotNull(it.arguments?.getInt("id"))
             Screen2(id = id)
         }
-
-
+         */
     }
-
 }
 
+@Serializable
+object MyScreen1
+
+@Serializable
+data class MyScreen2(val id:Int)
 
 @Composable
 fun Screen1 ( onClick:()-> Unit ){
